c27b5dbb5e5cc2289cf32eeddc188f2c17b9ac4c
Esmaeil Ahmadipour <software8686@gmail.com>
2020-04-02 00:59:08 +0430

    Commit> Add UI & Listener's

--- app/src/main/java/ir/ea2/kotlinmaterialdesign/MainActivity.kt
+++ app/src/main/java/ir/ea2/kotlinmaterialdesign/MainActivity.kt
@@ -1,12 +1,34 @@
 package ir.ea2.kotlinmaterialdesign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ac_main_btn_fadeIn.setOnClickListener {
+
+        }
+
+        ac_main_btn_fadeOut.setOnClickListener {
+
+        }
+
+        ac_main_btn_scale.setOnClickListener {
+
+        }
+
+        ac_main_btn_rotate.setOnClickListener {
+
+        }
+
+        ac_main_btn_scale_rotate.setOnClickListener {
+
+        }
     }
 }
